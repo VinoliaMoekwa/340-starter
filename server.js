3bad6900a84e954941e0499306ebc0558f909432
VinoliaMoekwa <moe23001@byui.edu>
2025-11-04 14:47:13 +0200

    initial commit

--- server.js
+++ server.js
@@ -2,18 +2,33 @@
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
  *******************************************/
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressEjsLayouts)
+app.set("layout", "./layouts/layout")// not at views root
+
 /* ***********************
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const expressEjsLayouts = require("express-ejs-layouts")
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+
+/* Index route */
+app.get("/", function (req, res){ res.render("index", {title: "Home"})})
+
+
 
 /* ***********************
  * Local Server Information
