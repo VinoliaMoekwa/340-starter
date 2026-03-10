d13c00f86c39967be54e0b623077204b3cbc1853
Vinolia <vinoliamoekwa@gmail.com>
2026-03-10 14:42:42 +0200

    . layouts

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 /* ***********************
  * Require Statements
  *************************/
+const expressLayouts = require("express-ejs-layouts")
 const express = require("express")
 const env = require("dotenv").config()
 const app = express()
@@ -13,6 +14,10 @@ const static = require("./routes/static")
 /* ***********************
  * Routes
  *************************/
+/*View Engine and Templates*/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout")
 app.use(static)
 
 /* ***********************
