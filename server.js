461a7d011ddaa50af08a24e4e46ee459f1f9c31d
VinoliaMoekwa <nmoekwa@byupathway.edu>
2026-03-15 02:27:00 +0200

    Assignement week 1

--- server.js
+++ server.js
@@ -10,20 +10,22 @@ const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-//Index route
-app.get("/", function(req,res) {
-  res.render("index", { title: "Home" })
-})
-app.use(express.static("public"));
 
 /* ***********************
  * Routes
  *************************/
+
 /*View Engine and Templates*/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout")
+app.use(express.static("public"))
 app.use(static)
+
+//Index route
+app.get("/", function(req,res) {
+   res.render("index", { title: "Home" })
+})
 
 /* ***********************
  * Local Server Information
