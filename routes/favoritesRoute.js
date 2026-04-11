c55b6e34aa4d2184e95e91d4abdefa07491b6589
VinoliaMoekwa <moe23001@byui.edu>
2026-04-11 20:50:05 +0200

    favoritesRoute

--- routes/favoritesRoute.js
+++ routes/favoritesRoute.js
@@ -2,23 +2,36 @@ const express = require("express");
 const router = express.Router();
 
 const favoritesController = require("../controllers/favoritesController");
+const utilities = require("../utilities");
 
 /* ***************************
  * Favorites Page (UI)
  * GET /favorites
  * ************************** */
-router.get("/", favoritesController.buildFavorites);
+router.get(
+  "/",
+  utilities.checkJWTToken,
+  favoritesController.buildFavorites
+);
 
 /* ***************************
  * Toggle Favorite (AJAX from JS)
  * POST /favorites/toggle
  * ************************** */
-router.post("/toggle", favoritesController.toggleFavorite);
+router.post(
+  "/toggle",
+  utilities.checkJWTToken,
+  favoritesController.toggleFavorite
+);
 
 /* ***************************
  * Remove Favorite (AJAX fallback / page button)
  * POST /favorites/remove
  * ************************** */
-router.post("/remove", favoritesController.removeFavorite);
+router.post(
+  "/remove",
+  utilities.checkJWTToken,
+  favoritesController.removeFavorite
+);
 
 module.exports = router;
